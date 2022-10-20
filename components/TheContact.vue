d5d8d2c7eec5b33925391b73ec8c7ff019f7b252
Alif Taufikhur Rahmat Aminuddin <alif.tr.aminuddin@gmail.com>
2022-10-20 10:36:06 +0700

    Update Section size so that we able to add some space in the bottom for back to top function

--- components/TheContact.vue
+++ components/TheContact.vue
@@ -1,20 +1,22 @@
 <template>
-  <div id="contact" class="relative">
-    <img
-      src="~images/contacts/bg.png"
-      alt=""
-      class="w-full object-fill sm:hidden"
-    >
-    <img
-      src="~images/contacts/bg-md.png"
-      alt=""
-      class="w-full object-fill hidden sm:block lg:hidden"
-    >
-    <img
-      src="~images/contacts/bg-lg.png"
-      alt=""
-      class="w-full object-fill hidden lg:block"
-    >
+  <div id="contact" class="relative overflow-x-hidden">
+    <div class="lg:w-[1420px] sm:w-[768px] md:w-[1024px] min-w-full">
+      <img
+        src="~images/contacts/bg.png"
+        alt=""
+        class="w-full object-fill sm:hidden"
+      />
+      <img
+        src="~images/contacts/bg-md.png"
+        alt=""
+        class="w-full object-fill hidden sm:block lg:hidden"
+      />
+      <img
+        src="~images/contacts/bg-lg.png"
+        alt=""
+        class="w-full object-fill hidden lg:block"
+      />
+    </div>
 
     <div class="absolute inset-0 text-white mt-24 lg:mt-20">
       <h2
@@ -37,40 +39,40 @@
           src="~images/contacts/map.png"
           alt=""
           class="w-full object-cover object-center sm:hidden"
-        >
+        />
 
         <img
           src="~images/contacts/map-md.png"
           alt=""
           class="w-full object-cover object-center hidden sm:block lg:hidden"
-        >
+        />
 
         <img
           src="~images/contacts/map-lg.png"
           alt=""
           class="w-full object-cover object-center hidden lg:block"
-        >
+        />
 
         <BaseContainer>
-          <hr class="border-b-[.7px] border-[#eeeeee]/50 mt-4 sm:mt-12">
+          <hr class="border-b-[.7px] border-[#eeeeee]/50 mt-4 sm:mt-12" />
         </BaseContainer>
       </div>
 
       <BaseContainer class="mt-7 sm:mt-11">
-        <div class="sm:grid grid-cols-2 lg:grid-cols-3">
+        <div class="sm:grid grid-cols-2 lg:grid-cols-3 gap-12">
           <img
             src="~images/logo-full.png"
             alt="SSP Logo"
             class="hidden sm:block col-span-2 lg:col-span-1"
-          >
+          />
 
           <div class="sm:mt-12 lg:mt-0">
             <h3 class="font-semibold text-xl sm:text-2xl">
               {{ $t("footer.address") }}
             </h3>
             <p class="font-medium opacity-90 text-sm sm:text-base mt-0.5">
-              Indonesia Stock Exchange Tower 2, 17th Floor <br>
-              Jl. Jend. Sudirman Kav 52-53 <br>
+              Indonesia Stock Exchange Tower 2, 17th Floor <br />
+              Jl. Jend. Sudirman Kav 52-53 <br />
               Jakarta, 12190 Indonesia
             </p>
 
@@ -82,18 +84,27 @@
             </a>
           </div>
 
-          <div class="mt-7 sm:mt-12 lg:mt-0 max-w-fit justify-self-end">
+          <div
+            class="
+              mt-7
+              sm:mt-12
+              lg:mt-0
+              max-w-fit
+              justify-self-end
+              lg:justify-self-start
+            "
+          >
             <h3 class="font-semibold text-xl sm:text-2xl">
               {{ $t("footer.contact") }}
             </h3>
             <p class="font-medium opacity-90 text-sm sm:text-base mt-0.5">
-              ssp.@salehmology.gmail.com <br>
+              ssp.@salehmology.gmail.com <br />
               +62 21 515 7799 — (No. Fax)
             </p>
 
             <div class="flex space-x-12 mt-2.5">
               <a v-for="socmed in socmedList" :key="socmed.id" href="#">
-                <img :src="socmed.image" :alt="socmed.alt">
+                <img :src="socmed.image" :alt="socmed.alt" />
               </a>
             </div>
           </div>
@@ -104,32 +115,32 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import BaseContainer from './BaseContainer.vue'
+import Vue from "vue";
+import BaseContainer from "./BaseContainer.vue";
 
 export default Vue.extend({
   components: { BaseContainer },
 
   computed: {
-    socmedList () {
+    socmedList() {
       return [
         {
           id: 1,
-          image: require('images/contacts/socmed/Instagram.png'),
-          alt: 'Instagram'
+          image: require("images/contacts/socmed/Instagram.png"),
+          alt: "Instagram",
         },
         {
           id: 2,
-          image: require('images/contacts/socmed/Linkedin.png'),
-          alt: 'Linkedin'
+          image: require("images/contacts/socmed/Linkedin.png"),
+          alt: "Linkedin",
         },
         {
           id: 3,
-          image: require('images/contacts/socmed/Youtube.png'),
-          alt: 'Youtube'
-        }
-      ]
-    }
-  }
-})
+          image: require("images/contacts/socmed/Youtube.png"),
+          alt: "Youtube",
+        },
+      ];
+    },
+  },
+});
 </script>
